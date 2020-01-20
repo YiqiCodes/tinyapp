80c5fe5bb99add81e72307af3cb410160b02fa8b
Tyler <yiqizhang@gmail.com>
2020-01-20 22:55:28 +0000

    render, post done

--- expressServer.js
+++ expressServer.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,9 +26,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//.RENDER IS ACTUALLY CREATING AND PULLING FROM URL DATABASE?
+// HOW IS URLS_INDEX KNOW THAT EXPRESSSERVER NEEDS IT?
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// WHY IS LONGURL THE KEY HERE OF THE OBJECT PRINTED??
+// I THOUGHT NODEMON MADE UPDATES REFLECT RIGHT AWAY??
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Okay"); // Respond with 'Ok' (we will replace this)
+});
+
+// wHY CAN'T I CONSOLE LOG ON GET? BUT I CAN ON POST??
+
+// WHEN I INSPECT ELEMENTS OF THIS URL IT IS NOT THE SAME STYLE AS LOGGED HERE?
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/u/:shortURL", (req, res) => {
